b01a9504850ff9ebf37eddbd6cb9320796821b67
ccgargantua <lovelygargantua@gmail.com>
2024-11-11 15:37:35 -0500

    Alignment test

--- test.c
+++ test.c
@@ -8,44 +8,57 @@
 
 void test_arena_create()
 {
-    Arena *arena = arena_create(1024);
-    TEST_EQUAL(arena->end - arena->begin, 1024);
+    constexpr size_t arena_size = 1024;
+
+    Arena *arena = arena_create(arena_size);
+    TEST_EQUAL(arena->end - arena->begin, arena_size);
     arena_destroy(arena);
 }
 
 
 void test_arena_alloc()
 {
-    const size_t arena_size = 1024;
+    constexpr size_t arena_size     = 1024;
+    constexpr size_t num_bytes      = 256;
+    constexpr size_t num_more_bytes = 64;
+
     Arena *arena = arena_create(arena_size);
 
-    const size_t num_bytes = 256;
-
-    __attribute__((unused))
-        char *bytes = arena_alloc(arena, num_bytes, 1);
+    char *bytes = arena_alloc(arena, num_bytes, 1);
 
     TEST_EQUAL(
         (ptrdiff_t)(arena->end - arena->begin),
         (ptrdiff_t)(arena_size - num_bytes));
 
-    const size_t num_more_bytes = 64;
 
-    __attribute__((unused))
-        int *more_data = arena_alloc(arena, num_more_bytes, sizeof(int));
+    int *more_data = arena_alloc(arena, num_more_bytes, sizeof(int));
 
     TEST_EQUAL(
         (ptrdiff_t)(arena->end - arena->begin),
         (ptrdiff_t)(arena_size - (num_more_bytes * sizeof(int) + num_bytes)));
+
+    TEST_EQUAL(
+        bytes - (char *)more_data,
+        num_bytes - num_bytes % sizeof(int));
     arena_destroy(arena);
 }
 
 
 void test_arena_alloc_aligned()
 {
-    Arena *arena = arena_create(1024);
-    char *alloc1 = arena_alloc_aligned(arena, 6, 1, 4);
-    char *alloc2 = arena_alloc_aligned(arena, 1, 1, 4);
-    TEST_EQUAL(alloc2, alloc1 - 3);
+    constexpr size_t arena_size         = 1024;
+    constexpr size_t allocation1        = 6;
+    constexpr size_t allocation2        = 1;
+    constexpr unsigned int alignment    = 4;
+    constexpr ptrdiff_t expected_offset = alignof(size_t) % allocation1 + allocation2;
+
+    Arena *arena = arena_create(arena_size);
+
+    __attribute__((unused))
+        char *alloc1 = arena_alloc_aligned(arena, allocation1, 1, alignment);
+    char *alloc2 = arena_alloc_aligned(arena, allocation2, 1, alignment);
+    TEST_EQUAL(alloc2, alloc1 - expected_offset);
+
     arena_destroy(arena);
 }
 
